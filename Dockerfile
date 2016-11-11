3560807933f33a348d1975da891db16414e62f5e
Leonenko <DLeonenko@kiev.luxoft.com>
2016-11-11 13:56:42 +0200

    Adding assets + hostname fix

--- Dockerfile
+++ Dockerfile
@@ -6,15 +6,8 @@ ENV        BANNER_TEXT=Docker-Nifi-1.0.0 \
            INSTANCE_ROLE=cluster-node \
            NODES_LIST=zoo-0.zk:2181,zoo-1.zk:2181,zoo-2.zk:2181 \
            MYID=N/A
-COPY       start_nifi.sh /${NIFI_HOME}/
-COPY       zookeeper.properties /${NIFI_HOME}/conf/
-COPY       bootstrap.conf /${NIFI_HOME}/conf/
-COPY       flow.xml.gz /${NIFI_HOME}/conf/
-COPY       jackson-annotations-2.6.0.jar /${NIFI_HOME}/lib/
-COPY       jackson-core-2.6.1.jar /${NIFI_HOME}/lib/
-COPY       jackson-databind-2.6.1.jar /${NIFI_HOME}/lib/
-COPY       newar.nifi.processors.launchrules-1.0.0.1_20161012161408.nar /${NIFI_HOME}/lib/
-COPY       newar.nifi.processors.parsemap-1.0.0.1_20161012133625.nar /${NIFI_HOME}/lib/
+COPY       docker-nifi/start_nifi.sh /${NIFI_HOME}/
+COPY       nifi-artifacts/ /${NIFI_HOME}/
 VOLUME     /opt/datafiles \
            /opt/scriptfiles \
            /opt/certs
--- start_nifi.sh
+++ start_nifi.sh
@@ -4,17 +4,21 @@ set -e
 
 do_site2site_configure() {
 #  sed -i "s/nifi\.ui\.banner\.text=.*/nifi.ui.banner.text=${BANNER_TEXT}/g" ${NIFI_HOME}/conf/nifi.properties
-  sed -i "s/nifi\.remote\.input\.host=.*/nifi.remote.input.host=$(/sbin/ip a |awk '/10.2/ { print $2 }' | awk -F/ '{print $1}' | sed 's/\./-/g').default.pod.cluster.local/g" ${NIFI_HOME}/conf/nifi.properties
+  sed -i "s/nifi\.remote\.input\.host=.*/nifi.remote.input.host=${HOSTNAME}.nifi-cluster/g" ${NIFI_HOME}/conf/nifi.properties
   sed -i "s/nifi\.remote\.input\.socket\.port=.*/nifi.remote.input.socket.port=10101/g" ${NIFI_HOME}/conf/nifi.properties
   sed -i "s/nifi\.remote\.input\.secure=true/nifi.remote.input.secure=false/g" ${NIFI_HOME}/conf/nifi.properties
 }
 
 do_cluster_node_configure() {
 # NiFi properties
-  sed -i "s/nifi\.web\.http\.host=.*/nifi.web.http.host=$(/sbin/ip a |awk '/10.2/ { print $2 }' | awk -F/ '{print $1}' | sed 's/\./-/g').default.pod.cluster.local/g" ${NIFI_HOME}/conf/nifi.properties
+  sed -i "s/nifi\.web\.http\.host=.*/nifi.web.http.host=${HOSTNAME}.nifi-cluster/g" ${NIFI_HOME}/conf/nifi.properties
   sed -i "s/nifi\.cluster\.protocol\.is\.secure=true/nifi.cluster.protocol.is.secure=false/g" ${NIFI_HOME}/conf/nifi.properties
   sed -i "s/nifi\.cluster\.is\.node=false/nifi.cluster.is.node=true/g" ${NIFI_HOME}/conf/nifi.properties
-  sed -i "s/nifi\.cluster\.node\.address=.*/nifi.cluster.node.address=$(/sbin/ip a |awk '/10.2/ { print $2 }' | awk -F/ '{print $1}' | sed 's/\./-/g').default.pod.cluster.local/g" ${NIFI_HOME}/conf/nifi.properties
+  if [ -z "$NIFI_USE_HOSTNAME"]; then
+    sed -i "s/nifi\.cluster\.node\.address=.*/nifi.cluster.node.address=${HOSTNAME}.nifi-cluster/g" ${NIFI_HOME}/conf/nifi.properties
+  else
+    sed -i "s/nifi\.cluster\.node\.address=.*/nifi.cluster.node.address=$HOSTNAME.$NAMESPACE/g" ${NIFI_HOME}/conf/nifi.properties
+  fi
   sed -i "s/nifi\.cluster\.node\.protocol\.port=.*/nifi.cluster.node.protocol.port=10201/g" ${NIFI_HOME}/conf/nifi.properties
 #  sed -i "s/nifi\.state\.management\.embedded\.zookeeper\.start=false/nifi.state.management.embedded.zookeeper.start=true/g" ${NIFI_HOME}/conf/nifi.properties
   sed -i "s/nifi\.zookeeper\.connect\.string=.*/nifi.zookeeper.connect.string=${NODES_LIST}/g" ${NIFI_HOME}/conf/nifi.properties
@@ -28,6 +32,12 @@ do_cluster_node_configure() {
 
 # Zookeeper properties
 #  sed -i "/^server\.1=/q" ${NIFI_HOME}/conf/zookeeper.properties; sed -i "s/^server\.1=.*/server.1=/g" ${NIFI_HOME}/conf/zookeeper.properties
+# DB Authentication
+  sed -i"" -e "s/dbcp.obx.url=.+/dbcp.obx.url=$DBCP_OBX_URL/" \
+           -e "s/dbcp.obx.usr=.+/dbcp.obx.usr=$DBCP_OBX_USR/" \
+           -e "s/dbcp.obx.pwd=.+/dbcp.obx.pwd=$DBCP_OBX_PWD/" \
+    ${NIFI_HOME}/conf/nifi_custom.properties
+  cp -a ${NIFI_HOME}/lib/bootstrap/jackson*.jar ${NIFI_HOME}/lib/
 }
 
 do_site2site_configure
