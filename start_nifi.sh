0bcdc1b6c53b9a92bcf6a922bc8bad28ef7b6065
Dmytro Leonenko <dleonenko@gravity4.com>
2017-03-29 13:19:04 +0300

    Kerberos authentication added

--- start_nifi.sh
+++ start_nifi.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -x
 
 set -e
 
@@ -12,33 +12,38 @@ ZK_ELECTION_PORT=${ZK_ELECTION_PORT:-3888}
 ZK_ROOT_NODE=${ZK_ROOT_NODE:-/nifi}
 NIFI_JAVA_XMX=${NIFI_JAVA_XMX:-1g}
 NIFI_JAVA_XMS=${NIFI_JAVA_XMS:-100m}
-
+CA_SERVER_NAME=${CA_SERVER_NAME:-ca-server}
+CA_TCP_PORT=${CA_TCP_PORT:-9114}
+NIFI_TOOLKIT_VERSION=${NIFI_TOOLKIT_VERSION:-1.1.2}
 SECURE=false;
-[ "$IS_SECURE" -eq "true" ] && SECURE=true
-[ "$EMBEDED_ZK" -eq "true" ] && ZK_MYID=1
-
+[ "$IS_SECURE" = "true" ] && SECURE=true
+[ "$EMBEDED_ZK" = "true" ] && ZK_MYID=1
+NP="${NIFI_HOME}/conf/nifi.properties"
 
 do_site2site_configure() {
-  sed -i "s/nifi\.remote\.input\.host=.*/nifi.remote.input.host=${HOSTNAME}${DOMAINPART}/g" ${NIFI_HOME}/conf/nifi.properties
-  sed -i "s/nifi\.remote\.input\.socket\.port=.*/nifi.remote.input.socket.port=${S2S_PORT}/g" ${NIFI_HOME}/conf/nifi.properties
-  sed -i "s/nifi\.remote\.input\.secure=true/nifi.remote.input.secure=$SECURE/g" ${NIFI_HOME}/conf/nifi.properties
+  sed -i "s/nifi\.remote\.input\.host=.*/nifi.remote.input.host=${HOSTNAME}${DOMAINPART}/g" $NP; ls -la $NP
+  sed -i "s/nifi\.remote\.input\.socket\.port=.*/nifi.remote.input.socket.port=${S2S_PORT}/g" $NP; ls -la $NP
+  sed -i "s/nifi\.remote\.input\.secure=.*/nifi.remote.input.secure=$SECURE/g" $NP; ls -la $NP
 }
 
 
 do_cluster_node_configure() {
-# NiFi properties
-  sed -i "s/nifi\.web\.http\.host=.*/nifi.web.http.host=${HOSTNAME}${DOMAINPART}/g" ${NIFI_HOME}/conf/nifi.properties
-  sed -i "s/nifi\.cluster\.protocol\.is\.secure=.*/nifi.cluster.protocol.is.secure=$SECURE/g" ${NIFI_HOME}/conf/nifi.properties
-  sed -i "s/nifi\.cluster\.is\.node=false/nifi.cluster.is.node=true/g" ${NIFI_HOME}/conf/nifi.properties
-  sed -i "s/nifi\.cluster\.node\.address=.*/nifi.cluster.node.address=${HOSTNAME}${DOMAINPART}/g" ${NIFI_HOME}/conf/nifi.properties
-  sed -i "s/nifi\.cluster\.node\.protocol\.port=.*/nifi.cluster.node.protocol.port=${NODE_PROTOCOL_PORT}/g" ${NIFI_HOME}/conf/nifi.properties
-
-  sed -i "s@nifi\.variable\.registry\.properties\=.*@nifi.variable.registry.properties=./conf/environment_properties/nifi_custom.properties@g" ${NIFI_HOME}/conf/nifi.properties
-
-  sed -i "s%nifi\.zookeeper\.connect\.string=.*%nifi.zookeeper.connect.string=${NODES_LIST}%g" ${NIFI_HOME}/conf/nifi.properties
-  sed -i "s@nifi\.zookeeper\.root\.node=.*@nifi.zookeeper.root.node=${ZK_ROOT_NODE}@g" ${NIFI_HOME}/conf/nifi.properties
-# State management
-  sed -i "s/<property name=\"Connect String\">.*</<property name=\"Connect String\">${NODES_LIST}</g" ${NIFI_HOME}/conf/state-management.xml
+  # Patching
+  cat >/tmp/patch1.nifi.properties <<EOF
+nifi.web.http.host=${HOSTNAME}${DOMAINPART}
+nifi.web.https.host=${HOSTNAME}${DOMAINPART}
+nifi.cluster.protocol.is.secure=$SECURE
+nifi.cluster.is.node=true
+nifi.cluster.node.address=${HOSTNAME}${DOMAINPART}
+nifi.cluster.node.protocol.port=${NODE_PROTOCOL_PORT}
+nifi.variable.registry.properties=./conf/environment_properties/nifi_custom.properties
+nifi.zookeeper.connect.string=${NODES_LIST}
+nifi.zookeeper.root.node=${ZK_ROOT_NODE}
+EOF
+  awk -F= 'NR==FNR{a[$1]=$0;next;}a[$1]{$0=a[$1]}1' /tmp/patch1.nifi.properties $NP > ${NP}.mod ;
+  cat ${NP}.mod > ${NP}
+  sed -i "s#<property name=\"Connect String\">.*<#<property name=\"Connect String\">${NODES_LIST}<#g" ${NIFI_HOME}/conf/state-management.xml
+  sed -i "s#\(authorizations.xml\)#efs/${HOSTNAME}${DOMAINPART}-\1#;s#\(users.xml\)#efs/${HOSTNAME}${DOMAINPART}-\1#" ${NIFI_HOME}/conf/authorizers.xml
   [ -n "${NIFI_LOG_DIR}" ] && mkdir -p ${NIFI_LOG_DIR}/${HOSTNAME}
 
 
@@ -47,7 +52,7 @@ do_cluster_node_configure() {
   sed -i "s/-Xms.*/-Xms$NIFI_JAVA_XMS/" ${NIFI_HOME}/conf/bootstrap.conf
 # MyId zookeeper
   if [ -n "$EMBEDED_ZK" ]; then
-    sed -i "s/nifi\.state\.management\.embedded\.zookeeper\.start=false/nifi.state.management.embedded.zookeeper.start=true/g" ${NIFI_HOME}/conf/nifi.properties
+    sed -i "s/nifi\.state\.management\.embedded\.zookeeper\.start=false/nifi.state.management.embedded.zookeeper.start=true/g" $NP
     mkdir -p ${NIFI_HOME}/state/zookeeper
     echo ${ZK_MYID} > ${NIFI_HOME}/state/zookeeper/myid
     # Zookeeper properties
@@ -55,20 +60,37 @@ do_cluster_node_configure() {
     srv=1; IFS=","; for node in $ZK_NODES; do sed -i "\$aserver.$srv=$node:${ZK_MONITOR_PORT}:${ZK_ELECTION_PORT}" ${NIFI_HOME}/conf/zookeeper.properties; ((srv++)); done
   fi
   sed -i "s/clientPort=.*/clientPort=${ZK_CLIENT_PORT}/g" ${NIFI_HOME}/conf/zookeeper.properties
-  # KeyStore and Truststore passwords
-  sed -i "s@nifi\.security\.keyPasswd=.*@nifi.security.keyPasswd=$NIFI_KEYSTORE_PASSWD@;s@nifi\.security\.keystorePasswd=.*@nifi.security.keystorePasswd=$NIFI_KEYSTORE_PASSWD@" ${NIFI_HOME}/conf/nifi.properties
-  sed -i "s@nifi\.security\.truststorePasswd=.*@nifi.security.truststorePasswd=$NIFI_TRUSTSTORE_PASSWD@" ${NIFI_HOME}/conf/nifi.properties
-  
-  # Kerberos auth
-  if [ "$SECURE" = "true" ];then
-    sed -i "s@nifi\.kerberos\.krb5\.file=.*@nifi.kerberos.krb5.file=./conf/environment_properties/krb5.conf@;s@nifi\.kerberos\.spnego\.keytab\.location=.*@nifi.kerberos.spnego.keytab.location=./conf/krb/krb5.keytab@" ${NIFI_HOME}/conf/nifi.properties
-    sed -i "s%nifi\.kerberos\.spnego\.principal=.*%nifi.kerberos.spnego.principal=$NIFI_KERBEROS_SPENGO_PRINCIPAL%" ${NIFI_HOME}/conf/nifi.properties
-  fi
+
+
+if [ "$SECURE" = "true" ]; then
+  if [ -z "${CA_SERVER_TOKEN}" ]; then echo "CA_SERVER_TOKEN variable must be configured. Get it from the CA server config.json stored on EFS" 1>&1 ; exit 1; fi
+
+  ${NIFI_HOME}/nifi-toolkit-${NIFI_TOOLKIT_VERSION}/bin/tls-toolkit.sh  client -c ${CA_SERVER_NAME} -t ${CA_SERVER_TOKEN} -p ${CA_TCP_PORT} -D "CN=${HOSTNAME}${DOMAINPART}, OU=NIFI"
+  KSP=$(grep keyStorePassword config.json | sed -e 's/.*: "\(.*\)",/\1/')
+  KP=$(grep keyPassword config.json | sed -e 's/.*: "\(.*\)",/\1/')
+  TP=$(grep trustStorePassword config.json | sed -e 's/.*: "\(.*\)",/\1/')
+  sed -i"" -e "s@\(nifi\.security\.keystore=\).*@\1/opt/nifi/keystore.jks@;s@\(nifi\.security\.keystoreType=\).*@\1jks@;s@\(nifi\.security\.keystorePasswd=\).*@\1${KSP}@;s@\(nifi\.security\.keyPasswd=\).*@\1${KP}@;s@\(nifi\.security\.truststore=\).*@\1/opt/nifi/truststore.jks@;s@\(nifi\.security\.truststoreType=\).*@\1jks@;s@\(nifi\.security\.truststorePasswd=\).*@\1${TP}@" $NP
+  for i in $(seq 0 9) ; do
+    ALLOW="${ALLOW}<property name=\"Node Identity $i\">CN=nifi-${i}${DOMAINPART}, OU=NIFI</property>"
+  done
+  sed -i"" -e "s@<property name=\"Node Identity 1\"></property>@$ALLOW@" ${NIFI_HOME}/conf/authorizers.xml
+  sed -i"" -e "s@# \(nifi\.security\.identity\.mapping\.pattern\.kerb\)@\1@;s@# \(nifi\.security\.identity\.mapping\.value\.kerb\)@\1@" $NP
+fi
+}
+
+do_properties_patching() {
+  PATCH_FILES=$(find $1 -follow -maxdepth 1 -type f)
+  for f in ${PATCH_FILES}; do
+  echo -e "========== PATCHING nifi.properties WITH CONFIGMAP PATCHES : $f ============ "
+  awk -F= 'NR==FNR{a[$1]=$0;next;}a[$1]{$0=a[$1]}1' $f $NP >$NP.mod;
+  cat $NP.mod >$NP
+  done
 }
 
 if [ -z "$DO_NOT_TOUCH_CONFIGS" ]; then
   do_site2site_configure
   do_cluster_node_configure
+  [ -d "$PATCH_NIFI_PROPERTIES_PATH" ] && do_properties_patching $PATCH_NIFI_PROPERTIES_PATH
 fi
 
 ${NIFI_HOME}/bin/nifi.sh run
